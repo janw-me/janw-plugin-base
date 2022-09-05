f4d2f321e4dbc1f6d783dcf8761b88e9e1b426e9
Jan Willem Oostendorp <webmaster@janw.me>
2022-09-05 14:55:29 +0200

    Made plugin file path relative.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -29,7 +29,9 @@ require_once "{$_tests_dir}/includes/functions.php";
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/janw-plugin-base.php';
+	$dir = dirname( dirname( __FILE__ ) );
+	$file_name = basename( dirname( dirname( __FILE__ ) ) );
+	require "$dir/$file_name.php";
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
